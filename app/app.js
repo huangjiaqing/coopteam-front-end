aed12df94145d6059fdac060cf19f6173d8c34d8
黄嘉庆 <657593825@qq.com>
2018-04-01 21:49:24 +0800

    增加了antd的支持

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'antd'
 
 const App = () => {
   console.log('环境: ', process.env.NODE_ENV)
 
   return (
     <div>
-      我是黄嘉庆哦
+      <Button type="primary">我是黄嘉庆哦</Button>
     </div>
   )
 }
--- config/webpack/webpack.base.js
+++ config/webpack/webpack.base.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HappyPack = require('happypack')
 const resolve = require('path').resolve;
 
 const root = (dir='') => resolve(__dirname, `../../${dir}`);
@@ -10,6 +11,8 @@ module.exports = {
 
   entry: {
     app: [
+      'normalize.css',
+      './app/utils/customize.css',
       './app/app.js'
     ]
   },
@@ -28,28 +31,42 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
         include: [root('app'), root('test')],
-        use: ['babel-loader']
+        // use: ['babel-loader?cacheDirectory']
+        use: ['happypack/loader?id=babel']
       },
+      {
+        test: /\.css$/,
+        include: [
+          root('app'),
+          root('node_modules/normalize.css')
+        ],
+        use: ['happypack/loader?id=css'],
+        // use: [
+        //   'style-loader',
+        //   {
+        //     loader: 'css-loader',
+        //     options: {
+        //       namedExport: true,
+        //       sourceMap: true,
+        //       importLoaders: 1,
+        //       module: true,
+        //       camelCase: true,
+        //       localIdentName: '[name]__[local]-[hash:base64:5]',
+        //       less: true
+        //     }
+        //   },
+        //   'postcss-loader'
+        // ]
+      },
+      // 不要对antd使用css模块化
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              namedExport: true,
-              sourceMap: true,
-              importLoaders: 1,
-              module: true,
-              camelCase: true,
-              localIdentName: '[name]__[local]-[hash:base64:5]',
-              less: true
-            }
-          },
-          'postcss-loader'
-      ]
+          'postcss-loader',
+        ],
+        include: [root('/node_modules/antd'),]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -76,6 +93,33 @@ module.exports = {
       template: resolve(__dirname, '../template/index.html'),
       inject: true
     }),
+		new HappyPack({
+      id: 'babel',
+      loaders: ['babel-loader?cacheDirectory']
+    }),
+    new HappyPack({
+      id: 'css',
+      loaders: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            namedExport: true,
+            sourceMap: true,
+            importLoaders: 1,
+            module: true,
+            camelCase: true,
+            localIdentName: '[name]__[local]-[hash:base64:5]',
+            less: true
+          }
+        },
+        'postcss-loader'
+      ]
+    }),
   ],
+
+  watchOptions: {
+    ignored: /node_modules/,
+  }
 
 }
